ec09644cc33e1500cc8300ead4463dfc27ca2aab
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-01-27 09:45:16 +0200

    3rd commit

--- assets/brain.js
+++ assets/brain.js
@@ -5,6 +5,13 @@
 //  const fs =require('./log')
 //  const add = fs.addition
 //  console.log(add(9,3));
+const path= require('node:path')
 
 
-console.log('i am here');
+
+console.log(path.basename(__dirname));
+console.log(path.parse(__dirname));
+console.log(path.isAbsolute(__dirname));
+console.log(path.join("folder1","folder2","module.html"));
+console.log(path.isAbsolute("folder1"));
+console.log(path.resolve('folder1',"folder2","module.html"));
